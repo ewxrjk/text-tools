e9640f95a14274a74cf69b29b46a7eaed3afcaf4
Richard Kettlewell <rjk@terraraq.org.uk>
2013-08-24 12:33:00 +0100

    Use autoreconf

--- autogen.sh
+++ autogen.sh
@@ -3,9 +3,4 @@ set -e
 srcdir=$(dirname $0)
 here=$(pwd)
 cd $srcdir
-mkdir -p config.aux
-aclocal
-autoconf
-autoheader
-automake -a || true		# for INSTALL
-automake --foreign -a
+exec autoreconf -si "$@"
